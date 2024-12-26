8277064bbce1ef3784681f666a912906f41cac84
Rohit Gupta <76804979+lucidrohit@users.noreply.github.com>
2024-12-26 19:54:03 +0530

    Update PushNotificationProvider.tsx

--- src/providers/PushNotificationProvider.tsx
+++ src/providers/PushNotificationProvider.tsx
@@ -1,29 +1,29 @@
-"use client";
-import { useEffect } from "react";
-import { register } from "@/serviceWorker";
-import useFcmToken from "@/hooks/useFcmToken";
-import { getMessaging, onMessage } from "firebase/messaging";
-import { app } from "@/config/firebaseConfig";
+// "use client";
+// import { useEffect } from "react";
+// import { register } from "@/serviceWorker";
+// import useFcmToken from "@/hooks/useFcmToken";
+// import { getMessaging, onMessage } from "firebase/messaging";
+// import { app } from "@/config/firebaseConfig";
 
-register();
+// register();
 
-export default function PushNotificationProvider({ children }: any) {
-  const { fcmToken, notificationPermissionStatus } = useFcmToken();
-  // Use the token as needed
-  fcmToken && console.log("FCM token:", fcmToken);
+// export default function PushNotificationProvider({ children }: any) {
+//   const { fcmToken, notificationPermissionStatus } = useFcmToken();
+//   // Use the token as needed
+//   fcmToken && console.log("FCM token:", fcmToken);
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      const messaging = getMessaging(app);
-      const unsubscribe = onMessage(messaging, (payload) => {
-        console.log("Foreground push notification received:", payload);
-      });
-      return () => {
-        unsubscribe(); // Unsubscribe from the onMessage event
-      };
-    }
-  }, []);
+//   // eslint-disable-next-line react-hooks/rules-of-hooks
+//   useEffect(() => {
+//     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+//       const messaging = getMessaging(app);
+//       const unsubscribe = onMessage(messaging, (payload) => {
+//         console.log("Foreground push notification received:", payload);
+//       });
+//       return () => {
+//         unsubscribe(); // Unsubscribe from the onMessage event
+//       };
+//     }
+//   }, []);
 
-  return children;
-}
+//   return children;
+// }
